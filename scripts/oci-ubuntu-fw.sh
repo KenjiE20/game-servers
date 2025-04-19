ca2749c07f94c051f24116a7a76b809938d542d1
Kenji Eva <KenjiE20@yahoo.co.uk>
2025-04-19 12:04:50 +0000

    add port range commands to firewall

--- scripts/oci-ubuntu-fw.sh
+++ scripts/oci-ubuntu-fw.sh
@@ -1,8 +1,12 @@
 # Minecraft
+# Swap commented lines for a port range for multiple servers
 sudo iptables -A INPUT -p tcp --dport 25565 -j ACCEPT
+#sudo iptables -A INPUT -p tcp --dport 25565:25570 -j ACCEPT
 sudo iptables -A INPUT -p udp --dport 25565 -j ACCEPT
+#sudo iptables -A INPUT -p udp --dport 25565:25570 -j ACCEPT
 # Minecraft Simple Voice Chat
 sudo iptables -A INPUT -p udp --dport 24454 -j ACCEPT
+#sudo iptables -A INPUT -p udp --dport 24454:24459 -j ACCEPT
 
 # Webpage
 sudo iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
